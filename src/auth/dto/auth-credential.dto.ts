b6312fc69d91c4be0b0327caa6e81447d182df61
buddy <jang_taehwan@naver.com>
2023-01-23 18:10:05 +0900

    feat: 유저 생성 validation pipe 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, Min, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]+$/, {
+    message: 'password only accepts a-z, A-Z, 0-9',
+  })
   password: string;
 }
