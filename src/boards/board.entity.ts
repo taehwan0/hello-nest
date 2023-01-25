c7449066616b878dfffb507f38f0d6a0448d5caf
buddy <jang_taehwan@naver.com>
2023-01-25 18:51:38 +0900

    refactor: user-board 연관 관계 추가

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,5 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BoardStatus } from './board-status.enum';
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Board extends BaseEntity {
@@ -14,6 +21,9 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  @ManyToOne(() => User, (user) => user.boards, { eager: false })
+  user: User;
 
   updateStatus(status: BoardStatus) {
     this.status = status;
